1e80c5b50da3844f3f5d1c4ef1184c6f6fedd7af
shubhchordia <131235104+shubhchordia@users.noreply.github.com>
2024-04-04 00:17:09 +0530

    Change project name

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -15,8 +15,8 @@ def generate_launch_description():
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
+    # Process the URDF filemy_bot
+    pkg_path = os.path.join(get_package_share_directory('mybot_one'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
